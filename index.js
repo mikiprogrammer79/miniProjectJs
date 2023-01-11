08ee91a1124a631c0bd3ce8777b30e177036485b
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2023-01-11 10:58:03 +0000

    Create JS pseudocode for contentEditable webpage

---
+++ index.js
@@ -0,0 +1,5 @@
+//Create the startEdit function and assing the contentEditable property to true
+
+//Create the stopEdit function and assing the contentEditable property to false
+
+//Call the function when the button is oncliked
