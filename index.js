a4cede476f3ac3aa5cc24785c0a1a3d413b59fcd
mikiprogrammer79 <115552497+mikiprogrammer79@users.noreply.github.com>
2023-01-11 11:23:44 +0000

    Create JS code for the contenteditable.html webpage

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 //Create the startEdit function and assing the contentEditable property to true
+function startEdit() {
+    let element = document.getElementById("editableElement");
+    element.contentEditable = true;  //Make element editable
+    element.focus();
+};
 
 //Create the stopEdit function and assing the contentEditable property to false
+function stopEdit() {
+    let element = document.getElementById("editableElement");
+    element.contentEditable = false; //Stop element to be editable
+    alert(element.innerHTML); //Show the markup in a message box
+}
 
 //Call the function when the button is oncliked
+document.getElementById("edit").onclick = function() {
+    startEdit();
+};
+document.getElementById("stop").onclick = function() {
+    stopEdit();
+};
